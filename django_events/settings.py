6ba6b5fa2c2999f9569d124dc07a90b7d325dad4
Dari <dari.k@abv.bg>
2020-12-12 19:26:29 +0200

    port changed

--- django_events/settings.py
+++ django_events/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
         'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT"),
+        'PORT': '5432',
     }
 }
 
