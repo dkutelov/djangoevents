81e9cf99b75eba5c114992043983e80694d1fa56
Dari <dari.k@abv.bg>
2020-12-12 19:30:00 +0200

    cities extraction commented out

--- events/forms/filter_form.py
+++ events/forms/filter_form.py
@@ -3,7 +3,8 @@ from django import forms
 from events.enums import EventTypesEnum
 from events.models import Event
 
-city_choices = [(0, 'By city')] + [(city['city'], city['city'])for city in Event.objects.all().values("city").distinct()]
+# city_choices = [(0, 'By city')] + [(city['city'], city['city'])for city in Event.objects.all().values("city").distinct()]
+city_choices = [(0, 'By city'), ('Sofia', 'Sofia'), ('Plovdiv', 'Plovdiv')]
 type_choices = [(0, 'By type')] + [(event_type.name, event_type.value) for event_type in EventTypesEnum]
 type_choices = [type for type in type_choices if type[0] != "UNKNOWN"]
 
