9b6c4c9db366d7e282542cb853123dcca6191f8e
Dari <dari.k@abv.bg>
2020-12-13 15:17:58 +0200

    comment form test

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+# from django.contrib.auth.models import User
+
+from events.forms.comment_form import CommentForm
+
+# user = User.objects.get(pk=42)
+
+
+# Form test
+class TestCommentForm(unittest.TestCase):
+    def test_valid_form(self):
+        data = {'text': 'Some text'}
+        self.form = CommentForm(data=data)
+        self.assertTrue(self.form.is_valid())
+
+    def invalid_valid_form_short_text(self):
+        data = {'text': 'S'}
+        self.form = CommentForm(data=data)
+        self.assertFalse(self.form.is_valid())
+
+    def invalid_valid_form_long_text(self):
+        data = {'text': 'Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. Some long text above 300 characters. '}
+        self.form = CommentForm(data=data)
+        self.assertFalse(self.form.is_valid())
+
